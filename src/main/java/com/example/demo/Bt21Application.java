6973cd00537b6127f2812589c02a85d0f3e39cf3
Demon07 <747576536@qq.com>
2018-05-24 11:41:26 +0800

    作业提交

--- src/main/java/com/example/demo/Bt21Application.java
+++ src/main/java/com/example/demo/Bt21Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,22 +12,27 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Bt21Application {
 
-	@Bean ApplicationRunner runner() {
-		return new ApplicationRunner () {
-		
-			
-			@Override
-			public void run(ApplicationArguments args) throws Exception {
-				// TODO Auto-generated method stub
-				System.out.println("刘鹏程--747576536@qq.com");
-			}
+	@Autowired
+	ApplicationContext context;
+	
+	@Bean
+	public CommandLineRunner runner() {
+		return (args) -> {
+			int count = context.getBeanDefinitionCount();
+	        System.out.println("Beans的个数：" + count);
+	        String[] names = context.getBeanDefinitionNames();
+	        for (String name : names) {
+	        		System.out.printf("%s -> %s\n", name, context.getBean(name));
+	        }
 		};
-		
 	}
+		
+	
+		
 	
 	public static void main(String[] args) {
 		/*SpringApplication.run(Bt2Application.class, args);*/
-		ApplicationContext context = SpringApplication.run(Bt21Application.class, args);
+		/*ApplicationContext context = SpringApplication.run(Bt21Application.class, args);
         int count = context.getBeanDefinitionCount();
         System.out.println("Beans的个数：" + count);
         String[] names = context.getBeanDefinitionNames();
@@ -34,6 +41,6 @@ public class Bt21Application {
                System.out.print("->");
                Object bean = context.getBean(name);
                System.out.println(bean.getClass().getName());
-        }
+        }*/
 	}
 }
